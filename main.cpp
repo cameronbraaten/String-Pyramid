c87961de3331165f34d45791159f5e094d9f5ff8
Cameron Braaten <cameronbraaten2017@gmail.com>
2021-01-13 19:56:00 +0000

    Doesn't allow user to enter '#' char, will only proceed if string does not include the '#' char.

--- main.cpp
+++ main.cpp
@@ -8,13 +8,27 @@ string user_string;
 string line_1;
 string ref_string;
 string pyramid;
+bool no_hash=false;
 
+while (no_hash==false){
 cout << "Please enter a string!" << endl;
 getline(cin,user_string);
+for (char c:user_string){
+  if (c=='#'){
+    cout << "Do not include the '#' character in your string!" << endl << endl;
+    cout << "Please enter a string!" << endl;
+    getline(cin,user_string);
+  }
+  else{
+    no_hash=true;
+  }
+}
+}
 
 for (int i=user_string.length()-2;i>=0;i--){
   line_1= line_1+user_string[i];
 }
+
 
 ref_string=user_string+line_1;
 
